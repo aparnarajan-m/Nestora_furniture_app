d9c994730064183268fc97804251e9a64d9f7efe
aparnarajan-m <aparna.meleveettil@gmail.com>
2025-09-29 16:03:39 +0530

    header.css importing mistake- Netlify

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import titleLogo from '../assets/titleLogo.png'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { openLogin, openLogout } from '../Redux/authSlice'
 import LogOutModal from './LogOutModal'
-import './header.css'
+import './header.css' 
 
 function Header() {
 
